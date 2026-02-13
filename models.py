69919657e898d1ce8008bc8b25cebb1fbbf9d2bf
Amir Hariri <a.r.hariri2000@gmail.com>
2026-02-12 23:07:55 -0400

    models

---
+++ models.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn; import torch.nn.functional as F
+
+from utils import *
+
+
+
+# ======== MODEL (EMBEDDING + PROJECTOR + CLASSIFIER HEAD) ========
+class CNN(nn.Module):
+    def __init__(self, ch=CH, seq=SEQ, emb_dim=128, proj_dim=128, dropout=DROPOUT):
+        super().__init__()
+        self.conv1 = nn.Conv1d(ch, 64, 8, padding="same")
+        self.conv2 = nn.Conv1d(64, 128, 6, padding="same")
+        self.conv3 = nn.Conv1d(128, 256, 4, padding="same")
+        self.pool = nn.AdaptiveAvgPool1d(1)
+        self.fc1 = nn.Linear(256, 256)
+        self.fc_emb = nn.Linear(256, emb_dim)  # embedding
+        self.drop = nn.Dropout(dropout)
+
+        # projector for VICReg
+        self.proj = nn.Sequential(
+            nn.Linear(emb_dim, emb_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(emb_dim, proj_dim))
+
+        # classifier head (set later for fine-tune)
+        self.classifier = None
+
+        self.apply(self._init)
+        print(f"Parameters count: {count_params(self):,}")
+
+    def _init(self, m):
+        if isinstance(m, (nn.Conv1d, nn.Linear)):
+            nn.init.kaiming_normal_(m.weight, nonlinearity="relu")
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+
+    def set_classifier(self, num_classes: int):
+        self.classifier = nn.Sequential(
+                        nn.Linear(self.fc_emb.out_features, 
+                                self.fc_emb.out_features // 2),
+                        nn.ReLU(),
+                        nn.Linear(self.fc_emb.out_features // 2, num_classes),
+                    )
+    
+    def set_linear_probe(self, num_classes: int):
+        self.classifier = nn.Linear(self.fc_emb.out_features, num_classes)
+
+    def forward(self, x, return_emb=False, return_proj=False):
+        x = F.relu(self.conv1(x))
+        x = self.drop(x)
+        x = F.relu(self.conv2(x))
+        x = self.drop(x)
+        x = F.relu(self.conv3(x))
+        x = self.drop(x)
+
+        x = self.pool(x).squeeze(-1)
+
+        x = F.relu(self.fc1(x))
+        x = self.drop(x)
+        emb = self.fc_emb(x)
+
+        if return_proj:
+            return self.proj(emb)
+        if return_emb:
+            return emb
+        return self.classifier(emb)
+   
